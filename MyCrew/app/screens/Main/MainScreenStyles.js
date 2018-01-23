3282155b00230678f6900334ac1fd7417260a9b4
Leonardo Fontes <leofontes@gmail.com>
2018-01-23 13:27:40 -0200

    logo and fix map size

--- MyCrew/app/screens/Main/MainScreenStyles.js
+++ MyCrew/app/screens/Main/MainScreenStyles.js
@@ -4,8 +4,8 @@ import { Dimensions, StyleSheet } from 'react-native'
 export default StyleSheet.create({
   map: {
     justifyContent: 'flex-end',
-    width: 400,
-    height: 400,
+    height: '100%',
+    width: '100%',
     alignItems: 'center'
   }
 });
